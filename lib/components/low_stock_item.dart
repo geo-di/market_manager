5e2ec4725eb52a9834a1e5155244f5a8496e4859
geo-di <georgellisdimos@gmail.com>
2025-07-02 21:50:59 +0300

    implemented page for showing products that are low in stock

---
+++ lib/components/low_stock_item.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:market_manager/components/reusable_card.dart';
+import 'package:market_manager/screens/show_product_screen.dart';
+import 'package:market_manager/screens/show_stock_screen.dart';
+
+class LowStockItem extends StatelessWidget {
+  LowStockItem({required this.barcode, required this.name, required this.quantity});
+
+  final String barcode;
+  final String name;
+  final int quantity;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Card(
+        color: Theme.of(context).colorScheme.primary,
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        elevation: 2,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+        child: Container(
+            width: double.infinity, // Full width
+            padding: const EdgeInsets.all(16),
+            child: Row(
+              children: [
+                // Left Column: barcode + name
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        barcode,
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.grey[700],
+                        ),
+                      ),
+                      const SizedBox(height: 4),
+                      Text(
+                        name.trim(),
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                // Right: Quantity in red
+                Text(
+                  quantity.toString(),
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.red,
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+
+      ),
+  onTap: () {
+    Navigator.pushNamed(
+      context,
+      ShowProductScreen.id,
+      arguments: {'barcode': barcode},
+    );
+  },
+    );
+  }
+}
---
+++ lib/screens/show_stock_screen.dart
@@ -0,0 +1,97 @@
+import 'dart:ffi';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:market_manager/components/low_stock_item.dart';
+import 'package:market_manager/utilities.dart';
+
+import 'chat_screen.dart';
+
+final _firestore = FirebaseFirestore.instance;
+late User loggedInUser;
+String? _sessionStore;
+
+
+class ShowStockScreen extends StatefulWidget {
+  const ShowStockScreen({super.key});
+
+  static const String id = 'show_stock';
+
+  @override
+  State<ShowStockScreen> createState() => _ShowStockScreenState();
+}
+
+class _ShowStockScreenState extends State<ShowStockScreen> {
+
+  final _auth = FirebaseAuth.instance;
+
+  @override
+  void initState() {
+    super.initState();
+
+    loggedInUser = Util.getCurrentUser(_auth, context);
+    if(_sessionStore==null){
+      _sessionStore = Hive.box('session').get('store');
+    }
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Low Stock"),
+      ),
+      body: SafeArea(
+          child: StreamBuilder(
+              stream: _firestore
+                  .collection('stores')
+                  .doc(_sessionStore)
+                  .collection('products')
+                  .where('track_stock', isEqualTo: true)
+                  .where('stock_ratio', isLessThan: 0.5)
+                  .orderBy('stock_ratio', descending: false)
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(
+                    child: CircularProgressIndicator(
+                        backgroundColor: Theme
+                            .of(context)
+                            .colorScheme
+                            .primary
+                    ),
+                  );
+                } else {
+                  final products = snapshot.data!.docs;
+
+                  List<LowStockItem> productItems = [];
+                  for (var product in products) {
+                    final productName = product.get('name');
+                    final productBarcode = product.id;
+                    final productQuantity = product.get('quantity');
+
+                    final productItem = LowStockItem(
+                      barcode: productBarcode,
+                      name: productName,
+                      quantity: productQuantity,
+
+                    );
+                    productItems.add(productItem);
+                  }
+
+                  return ListView(
+                    padding: EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 20
+                    ),
+                    children: productItems,
+                  );
+                }
+              }
+            ),
+      ),
+    );
+  }
+}
